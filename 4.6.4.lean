c46dc53adde6f4737f016b0662f0e291f8bd537e
Mike Plotz <mike@hyponymo.us>
2018-10-19 17:54:17 -0700

    4.6.4

--- 4.6.4.lean
+++ 4.6.4.lean
@@ -8,25 +8,33 @@ def even (n : ℕ) : Prop := 2 ∣ n
 
 -- BEGIN
 def prime (n : ℕ) : Prop :=
-sorry
+¬∃ m, m > 1 ∧ m < n ∧ (m ∣ n)
 
 def infinitely_many_primes : Prop :=
-sorry
+∀ n, ∃ p, p > n ∧ prime p
+
+def Fermat_number (n : ℕ) : Prop :=
+∃ k : ℕ, 2^(2^k) + 1 = n
 
 def Fermat_prime (n : ℕ) : Prop :=
-sorry
+prime n ∧ Fermat_number n
 
 def infinitely_many_Fermat_primes : Prop :=
-sorry
+∀ n, ∃ fp, fp > n ∧ Fermat_prime fp
 
+-- Every even integer greater than 2 can be expressed as the sum of two primes
 def goldbach_conjecture : Prop :=
-sorry
+∀ n, n > 2 → ∃ p q, p + q = n ∧ prime p ∧ prime q
 
+-- Every odd number greater than 5 can be expressed as the sum of three primes
 def Goldbach's_weak_conjecture : Prop :=
-sorry
+∀ n, even n ∧ n > 5 → ∃ p p' p'', p + p' + p'' = n ∧ prime p ∧ prime p' ∧ prime p''
 
+-- no three positive integers a, b, and c satisfy the equation an + bn = cn for
+-- any integer value of n greater than 2
 def Fermat's_last_theorem : Prop :=
-sorry
+∀ n, n > 2 → ¬∃ a b c, a^n + b^n = c^n ∧ a > 0 ∧ b > 0 ∧ c > 0
+
 -- END
 
 end hidden
