b72aba837be4bce926296ffc57cba46af4dfcd7d
Mike Plotz <mike@hyponymo.us>
2018-10-21 18:35:38 -0700

    4.6.7

--- 4.6.7.lean
+++ 4.6.7.lean
@@ -1,4 +1,8 @@
 #check sub_self
 
 example (x : ℤ) : x * 0 = 0 :=
-sorry
+calc
+    x * 0 = x * (x - x)   : by rw sub_self
+      ... = x * x - x * x : by rw mul_sub
+      ... = 0             : by rw sub_self
+
