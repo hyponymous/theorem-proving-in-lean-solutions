bdd00dd1032860235097852e25061fd893e85363
Mike Plotz <mike@hyponymo.us>
2018-10-21 18:31:00 -0700

    4.6.6

--- 4.6.6.lean
+++ 4.6.6.lean
@@ -17,4 +17,8 @@ exp_log_eq h
 
 theorem log_mul {x y : real} (hx : x > 0) (hy : y > 0) :
   log (x * y) = log x + log y :=
-sorry
+calc
+    log (x * y) = log (exp (log x) * exp (log y)) : by rw [exp_log_eq hx, exp_log_eq hy]
+            ... = log (exp (log x + log y))       : by rw exp_add
+            ... = log x + log y                   : by rw log_exp_eq
+
